4823da268634dc3d880505a3d94c65a9e05961c4
ralphp1121 <ralphp1121@hotmail.com>
2021-05-24 11:22:29 +0800

    completed the validation project 24-May-2021

--- script.js
+++ script.js
@@ -20,13 +20,27 @@ function showSuccess(input, message) {
 }
 
 //Check email is valid
+/*
+//old function to validate email
 function isValidEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+*/
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) { 
+        showSuccess(input);
+    }
+    else {
+        showError(input, 'Email is not valid');
+    }
+}
 
 
 //Check required fields
+// Note: using ${} syntax
+// This is to allow you to put a function inside the outside function parameter
 function checkRequired(inputArr) {
     inputArr.forEach(function(input) {
         if(input.value.trim() === '') {
@@ -38,7 +52,30 @@ function checkRequired(inputArr) {
     });
 }
 
+//Check input length
+function checkLength(input,min,max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    }
+    else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    }
+    else {
+        showSuccess(input);
+    }
+}
+
+//Check passwords match
+function checkPasswordsMatch(input1,input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }
+}
+
 //Get fieldname and uppercase
+// Note:
+// We use to .toUpperCase() function to make the first character CAPITAL letter and then the remaining string
+// as by default small letter using the .slice() function
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
@@ -48,7 +85,13 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
-/*    
+    checkLength(username,3,15);
+    checkLength(password,6,25);
+    checkLength(password2,6,25);
+    checkEmail(email);
+    checkPasswordsMatch(password,password2);
+/*  
+//This is the old long format of the function checker
     if (username.value == '') {
         showError(username, 'Username is required');
     } 
